85804fce8d92caa787c65e1f29ea004582f12ad7
h7x4 <h7x4@nani.wtf>
2024-01-27 07:33:47 +0100

    lib: fix bug where `lib.throw` does not exist

--- lib.nix
+++ lib.nix
@@ -4,7 +4,7 @@ rec {
   isListenerType = type: l: lib.any (r: lib.any (n: n == type) r.names) l.resources;
   # Get the first listener that includes the given resource from worker
   firstListenerOfType = type: ls: lib.lists.findFirst (isListenerType type)
-    (lib.throw "No listener with resource: ${type} configured")
+    (throw "No listener with resource: ${type} configured")
     ls;
   # Get an attrset of the host and port from a listener 
   connectionInfo = l: {
