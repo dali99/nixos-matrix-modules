da9dc0479ffe22362793c87dc089035facf6ec4d
Daniel Olsen <daniel.olsen99@gmail.com>
2025-01-02 23:15:03 +0100

    sliding-sync: remove

--- module.nix
+++ module.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
     ./synapse-module
-    ./sliding-sync
+
+    # TODO: Remove after 25.05
+    (lib.mkRemovedOptionModule [ "services" "matrix-synapse" "sliding-sync" ] ''
+      `services.matrix-synapse.sliding-sync` is no longer necessary to use sliding-sync with synapse.
+      As synapse now includes this in itself, if you have a manually managed `.well-known/matrix/client` file
+      remove the proxy url from it.
+    '')
   ];
 }
